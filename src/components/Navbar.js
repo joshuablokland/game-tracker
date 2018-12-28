9021421a7bc4f48575f043bc6d0251a3d2cb8d66
Joshua Blokland <shua.blokland@gmail.com>
2018-12-28 16:46:51 +0100

    Create the basic navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav class="navbar navbar-dark bg-primary">
+        <a class="navbar-brand" href="http://localhost:3000">Game Tracker</a>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
