3d602862e52e9e134b9e05edd6332ebc4e49de9d
Joshua Blokland <shua.blokland@gmail.com>
2019-02-08 15:29:52 +0100

    Added extra spacing on title

--- src/views/Account.js
+++ src/views/Account.js
@@ -7,7 +7,7 @@ class Account extends Component {
       <div className="container mt-5">
         <div className="row">
           <div className="col-md-8 offset-md-2">
-            <h1>Account settings</h1>
+            <h1 className="mb-4">Account settings</h1>
             <ProfileForm />
           </div>
         </div>
