af5013bd8933fc80eb7265659e418995a442e9ff
Joshua Blokland <shua.blokland@gmail.com>
2019-02-10 20:05:43 +0100

    It basically works from the front end

--- src/views/Search.js
+++ src/views/Search.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react'
 
-const IGDB = {
-  url: process.env.REACT_APP_IGDB_URL,
-  key: process.env.REACT_APP_IGDB_API_KEY
-}
-
 class Search extends Component {
 
   constructor(props) {
     super(props)
 
-    fetch('https://api-v3.igdb.com/games?search=Halo', {
-      mode: "cors",
-      headers: {
-        'user-key': IGDB.key
-      }
-    }).then(result => result).then(response => console.log(response))
+    this.state = {
+      searchResults: null
+    }
+
+    this.getSearchResults()
+  }
+
+  getSearchResults() {
+    fetch('http://localhost:5000/search/mario')
+      .then(r => r.json())
+      .then(d => {
+        this.setState({searchResults: d})
+      })
+      .catch(e => e)
   }
 
   render () {
     return (
       <div>
-        search
+        {JSON.stringify(this.state.searchResults)}
       </div>
     )
   }
