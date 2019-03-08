c1534afb9560c41fb1c8916fc34bc0e4f9eb2be2
Joshua Blokland <shua.blokland@gmail.com>
2019-03-08 13:15:21 +0100

    save to owned button SearchResult

--- src/components/SearchResult/index.js
+++ src/components/SearchResult/index.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
+import { withFirebase } from '../../firebase'
+import { connect } from 'react-redux'
 
 class SearchResult extends Component {
+  
+  onClick = event => {
+    this.props.firebase.doCreateRentalList(this.props.userUID, this.props.gameID)
+  }
+
   render() {
     return (
       <div className="card mb-4">
         <div className="card-body">
-          <h5 className="card-title">{this.props.title}</h5>      
+          <h5 className="card-title">{this.props.title}</h5>
+          {this.props.userLoggedIn && <button className="btn btn-md btn-primary" onClick={this.onClick}>Save to owned</button>}
         </div>
       </div>
     )
   }
 }
 
-export default SearchResult
+const mapStateToProps = (state) => ({ 
+  userLoggedIn: state.userLoggedIn,
+  userUID: state.user.uid
+})
+
+export default connect(mapStateToProps)(withFirebase(SearchResult))
